9ba780a99534dd30ac757a327838466966b92fdd
João Luis - DevRox <joaoluis.pasquarelli@yahoo.com>
2019-05-06 14:13:13 -0300

    Update read_docx.py

--- 0001 - Leitura de Arquivos .Docx/read_docx.py
+++ 0001 - Leitura de Arquivos .Docx/read_docx.py
@@ -1,14 +1,13 @@
 
-
 # Must have installed docx on yout wheel '-pip install docx'
 import docx
+
 import os
 import pprint
 
 
-
 # Consumes docx file into ::var:doc
-doc = docx.Document('C:/CLEAR-CASE-VOBS/SIART-20180108/SIART/01-Requisitos/Especificacoes/SIART_Espec_UC_Adiantamento_de_Valores_Provisionados.docx')
+doc = docx.Document('document_path/document.docx')
 #pprint.pprint(doc.paragraphs)
 
 

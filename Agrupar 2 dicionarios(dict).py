6476c754f4b371d5266d87bd76818169db230368
João Luis - DevRox <joaoluis.pasquarelli@yahoo.com>
2019-05-06 14:31:43 -0300

    Create Agrupar 2 dicionarios(dict).py

---
+++ Agrupar 2 dicionarios(dict).py
@@ -0,0 +1,13 @@
+
+# declara dicionario1 simples
+dicionario = {'1':'001','2':'002'}
+print(dicionario)
+
+
+# declara dicionario2 simples
+dicionario2 = {'3':'003','4':'004'}
+
+
+# atualiza o primeiro dicionario com informações do segundo (MERGE)
+dicionario.update(dicionario2)
+print(dicionario)

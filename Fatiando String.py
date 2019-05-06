ef9463197c336e2d4301267485018a9f6f55a04f
João Luis - DevRox <joaoluis.pasquarelli@yahoo.com>
2019-05-06 14:20:30 -0300

    Create Fatiando String.py

---
+++ Fatiando String.py
@@ -0,0 +1,20 @@
+
+# Este exercicio visa a manipulação de uma String Alfanumérica
+# com objetivo de verificar varias diferentes manipulações em Fatiamento
+
+palavra = 'paralelepipedo'
+
+# inicio na casa 4 ate a casa 7
+print(palavra[3:7])
+
+# primeiras 2 chars
+#print(palavra[0:2])
+
+# ultimas 2 chars
+#print(palavra[:2])
+
+# a partir da casa 3 devido ao índice iniciar no 0
+#print(palavra[2:])
+
+# exibir ate a penultimo chars
+#print(palavra[0:-2])
